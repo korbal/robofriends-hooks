a7114ea56667ff20b2c3bd6b8f3113648f1dae1a
Balint Korosi <balint.j.korosi@gmail.com>
2023-01-18 08:41:25 +0100

    hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, {  useState, useEffect } from "react";
 import CardList from "../components/CardList";
 
 import SearchBox from "../components/SearchBox";
@@ -7,43 +7,61 @@ import '../containers/App.css'
 
 import ErrorBoundry from "../components/ErrorBoundry";
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      robots: [],
-      searchfield: ""
-    };
+function App() {
+
+const [robots, setRobots] = useState([]);
+const [searchfield, setSearchfield] = useState("");
+const [count, setCount] = useState(0);
+// setCount is increased by 1 via the button onClick event. it's added to the useEffect dependency array, so the useEffect function is called again when the count is increased.
+
+useEffect(() => {
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => {
+    return response.json()
+  })  
+  .then(users =>{
+    setRobots(users)
+    console.log(count)
+  })
+}, [count]) // only run when count changes
+
+  // constructor() {
+  //   super();
+  //   this.state = {
+  //     robots: [],
+  //     searchfield: ""
+  //   };
+  // }
+
+  // componentDidMount(){
+  //   fetch('https://jsonplaceholder.typicode.com/users')
+  //   .then(response => {
+  //     return response.json()
+  //   })
+  //   .then(users =>{
+  //     this.setState({robots: users})
+  //   })
+  // }
+
+  const onSearchChange = (event) => {
+    //this.setState({searchfield: event.target.value})
+    setSearchfield(event.target.value);
   }
 
-  componentDidMount(){
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => {
-      return response.json()
+  
+    const filteredRobots = robots.filter(robot => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
-    .then(users =>{
-      this.setState({robots: users})
-    })
-    
-  }
+    //console.log(robots, searchfield);
 
-  onSearchChange = (event) => {
-    this.setState({searchfield: event.target.value})
-    
-  }
-
-  render() {
-    const filteredRobots = this.state.robots.filter(robot => {
-      return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-    })
-
-    if(this.state.robots.length === 0){
+    if(robots.length === 0){
       return <h1 className="tc">Loading...</h1>
     } else {
   return (
     <div className="tc container">
       <h1 className="f1">RoboFriends</h1>
-      <SearchBox searchChange={this.onSearchChange} />
+      <button onClick={() => setCount(count + 1)} >Click me!</button>
+      <SearchBox searchChange={onSearchChange} />
       <Scroll>  
         <ErrorBoundry>
           <CardList robots={filteredRobots} />
@@ -53,6 +71,6 @@ class App extends Component {
   );
     }
 }
-}
+
 
 export default App;
